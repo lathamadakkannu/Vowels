fbb5559a64a1d88f02a1ae9a253da03e4539d601
lathamadakkannu <lathamadakkannu@gmail.com>
2023-07-27 16:24:46 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,16 @@
+var a=prompt("Enter only string");
+
+document.write("Given string = "+a+"<br><br>");
+
+b=a.split("");
+var c=[];
+
+for (i=0; i<b.length; i++){
+    if (b[i]=="a"||b[i]=="e"||b[i]=="i"||b[i]=="o"||b[i]=="u"){
+        c.push(b[i]);
+    }
+
+}
+
+document.write("vowels in given array = "+c+"<br><br>");
+document.write("Number of vowels in given array ="+c.length);
